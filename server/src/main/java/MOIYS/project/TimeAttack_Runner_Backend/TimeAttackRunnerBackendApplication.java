99d835cc5464f5bad15683182ff6ab2c0ede0650
MOIYS <leajaehuck3@gmail.com>
2025-11-08 22:10:28 +0900

    chore: 초기 프로젝트 구조

---
+++ server/src/main/java/MOIYS/project/TimeAttack_Runner_Backend/TimeAttackRunnerBackendApplication.java
@@ -0,0 +1,13 @@
+package MOIYS.project.TimeAttack_Runner_Backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimeAttackRunnerBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TimeAttackRunnerBackendApplication.class, args);
+	}
+
+}
---
+++ server/src/test/java/MOIYS/project/TimeAttack_Runner_Backend/TimeAttackRunnerBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package MOIYS.project.TimeAttack_Runner_Backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TimeAttackRunnerBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
