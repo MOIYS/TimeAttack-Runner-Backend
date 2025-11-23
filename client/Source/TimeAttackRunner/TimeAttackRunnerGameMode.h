e32f008a1c44f50bc4b67b2ed4e0d9ab8f728c7b
MOIYS <leajaehuck3@gmail.com>
2025-11-24 01:38:08 +0900

    init: 언리얼(client) 프로젝트 생성

---
+++ client/Source/TimeAttackRunner/TimeAttackRunnerGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "TimeAttackRunnerGameMode.generated.h"
+
+UCLASS(minimalapi)
+class ATimeAttackRunnerGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	ATimeAttackRunnerGameMode();
+};
+
+
+
